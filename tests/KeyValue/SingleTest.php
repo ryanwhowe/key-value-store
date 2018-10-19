81ff431421891d6c2912ba60b1f1e178dbba63cd
Ryan W. Howe <rhowe@ivesinc.com>
2018-10-19 12:31:35 -0400

    * Removed grouping from the missed getGroupingMethods

--- tests/KeyValue/SingleTest.php
+++ tests/KeyValue/SingleTest.php
@@ -81,7 +81,7 @@ class SingleTest extends DataTransaction {
                 $singleValue->set($key, $value);
                 $expected_value = $value; // we expect the last value set
             }
-            $expected[] = array('grouping' => $testGroup, 'key' => \strtolower($key), 'value' => $expected_value);
+            $expected[] = array('key' => \strtolower($key), 'value' => $expected_value);
         }
 
         $result = $singleValue->getGroupingSet();
